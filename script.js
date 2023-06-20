9d266f4b5ca56833b6a37655535fb3d4bb474e59
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2023-06-20 17:50:22 +0530

    Update grids with  user input

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let userInput = prompt("Enter the grid size");
+
 const sketchPad = document.getElementById('sketchPad');
 
-function setupGrid() {
+
+/*function setupGrid() {
   
   for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
@@ -13,6 +16,21 @@ function setupGrid() {
     console.log("working");
 }    
 
-setupGrid();
+setupGrid();*/
+
+function updateGrid(userInput) {
+  for(let i =0;i<userInput * userInput;i++){
+    const div = document.createElement('div');
+    div.classList.add('grid');
+    div.addEventListener('mouseover', function(event){
+        event.target.style.backgroundColor = 'black';
+    })
+    sketchPad.appendChild(div);
+    sketchPad.style.gridTemplateColumns = `repeat(${userInput}, 2fr)`;
+    sketchPad.style.gridTemplateRows = `repeat(${userInput}, 2fr)`; 
+  }
+}
+
+updateGrid(userInput);
 
 
