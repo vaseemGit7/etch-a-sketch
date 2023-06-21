69842d9926cc07c4276870b330c2690f03b0474b
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2023-06-21 18:11:31 +0530

    Get DOM elements

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let userInput = prompt("Enter the grid size");
+let DEFAULT_COLOR = '#00000';
+let DEFAULT_SIZE = 16;
+let DEFAULT_MODE = 'color';
 
+let currentSize = DEFAULT_SIZE;
+let currentMode = DEFAULT_MODE;
+let currentColor = DEFAULT_COLOR;
+
+const colorMode = document.getElementById('colorMode');
+const randomMode = document.getElementById('randomMode');
+const eraser = document.getElementById('eraser');
+const clear = document.getElementById('clear');
+const colorPicker = document.getElementById('colorPicker');
+const slider = document.getElementById('slider');
+const sliderValue = document.getElementById('sliderValue');
+const gridToggle = document.getElementById('gridToggle');
 const sketchPad = document.getElementById('sketchPad');
 
-/*function setupGrid() {
-  
-  for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('grid');
-    div.addEventListener('mouseover', function(event){
-        event.target.style.backgroundColor = 'black';
-    })
-    sketchPad.appendChild(div); 
-}
-    console.log("working");
-}    
-
-setupGrid();*/
 
 function updateGrid(userInput) {
   for(let i =0;i<userInput * userInput;i++){
